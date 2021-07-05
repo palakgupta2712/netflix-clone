5b11b8aae0478e936de6e464aa87b1c35d462e24
Palak Gupta <palakgupta2712@gmail.com>
2021-07-05 10:40:25 +0530

    fix

--- src/pages/Trailer.jsx
+++ src/pages/Trailer.jsx
@@ -10,7 +10,7 @@ function Trailer() {
           width="100%"
           height="100%"
           loop="1"
-          src="https://www.youtube.com/embed/Hs-1_HNALhw?rel=0&modestbranding=1&autohide=1&showinfo=0&controls=0&loop=1"
+          src="https://www.youtube.com/embed/Hs-1_HNALhw?rel=0&modestbranding=1&&showinfo=0&controls=0&loop=1"
           frameBorder="0"
           allowFullScreen
         ></iframe>
